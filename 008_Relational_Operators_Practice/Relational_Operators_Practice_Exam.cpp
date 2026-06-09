91b30b3a3f1b77125f0f4bc362e00590e4b10d47
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-09 09:09:12 +0200

    Build C++ program to demonstrate comparison operators and boolean logic output

---
+++ 008_Relational_Operators_Practice/Relational_Operators_Practice_Exam.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int num1, num2;
+    cout << "Please Enter The First Number: ";
+    cin >> num1;
+
+    cout << "Please Enter The Secound Number: ";
+    cin >> num2;
+
+
+    cout << num1 << " = " << num2 << " IS: " << (num1 == num2) << endl;
+    cout << num1 << " != " << num2 << " IS: " << (num1 != num2) <<endl;
+    cout << num1 << " > " << num2 << " IS: " << (num1 > num2) <<endl;
+    cout << num1 << " < " << num2 << " IS: " << (num1 < num2) <<endl; 
+    cout << num1 << " >= " << num2 << " IS: " << (num1 >= num2) <<endl; 
+    cout << num1 << " <= " << num2 << " IS: " << (num1 <= num2) <<endl; 
+
+    cout << "-------------------------------\n";
+
+    if (num1 == num2)
+        cout << num1 << " = " << num2 << " IS: True. " <<endl;
+    else
+        cout << num1 << " = " << num2 << " IS: False." <<endl;
+
+    if (num1 != num2)
+        cout << num1 << " != " << num2 << " IS: True. " <<endl;
+    else
+        cout << num1 << " != " << num2 << " IS: False." <<endl;
+
+    if (num1 > num2)
+        cout << num1 << " > " << num2 << " IS: True. " <<endl;
+    else
+        cout << num1 << " > " << num2 << " IS: False." <<endl;
+
+    if (num1 < num2)
+        cout << num1 << " < " << num2 << " IS: True. " <<endl;
+    else
+        cout << num1 << " < " << num2 << " IS: False." <<endl;
+
+    if (num1 >= num2)
+        cout << num1 << " >= " << num2 << " IS: True. " <<endl;
+    else
+        cout << num1 << " >= " << num2 << " IS: False." <<endl;
+
+    if (num1 <= num2)
+        cout << num1 << " <= " << num2 << " IS: True. " <<endl;
+    else
+        cout << num1 << " <= " << num2 << " IS: False." <<endl;
+
+
+    return 0;
+}
