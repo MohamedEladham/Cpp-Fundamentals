e6d40e6187c0342f14473900f3318e67bcc804f1
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-09 23:35:31 +0200

    Demonstrate function declaration and invocation in C++

---
+++ 015_Basic_Functions_Practice/Basic_Functions_Practice_Exam.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void PrintMyInformation()
+{
+    cout << "\n\n*************************************\n";
+    cout << "Name: Mohammed Eladham\n";
+    cout << "Age : 27 Years\n";
+    cout << "City: Mansoura\n";
+    cout << "Country: Egypt\n";
+    cout << "*************************************\n";
+}
+
+void PrintStars()
+{
+    cout << "\n\n*********************\n";
+    cout << "*********************\n";
+    cout << "*********************\n";
+    cout << "*********************\n\n";
+}
+
+void PrintILoveProgramming()
+{
+    cout << "I Love Programming\n";
+    cout << "I promise to become the best developer I can be!\n";
+    cout << "I know it will take some time to practice \n\n";
+}
+
+void Print_H()
+{
+    cout << "*       *\n";
+    cout << "*       *\n";
+    cout << "*       *\n";
+    cout << "* * * * *\n";
+    cout << "*       *\n";
+    cout << "*       *\n";
+    cout << "*       *\n";
+}
+
+int main()
+{
+    PrintMyInformation();
+    PrintStars();
+    PrintILoveProgramming();
+    Print_H();
+    return 0;
+}
